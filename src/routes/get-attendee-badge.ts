e435c1943aa079cff75104ca0f1a2190dd2f0721
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-04-06 18:47:27 -0300

    criado a rota getAttendeeBadge

---
+++ src/routes/get-attendee-badge.ts
@@ -0,0 +1,43 @@
+// serve para retornar o cracha de um participante dentro de um evento
+
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { request } from "http";
+
+export async function getAttendeeBadge(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/attendees/:attendeeId/badge", {
+        schema: { 
+            params: z.object({
+                attendeeId: z.coerce.number().int(),
+            }),
+            response: {},
+        }
+    }, async (request, reply) => {
+        const { attendeeId } = request.params
+
+        const attendee = await prisma.attendee.findUnique({
+            select: {
+                name: true,
+                email: true,
+                event: {
+                    select: {
+                        title: true,
+                    }
+                }
+            },
+            where: {
+                id: attendeeId
+            }
+        })
+
+        if (attendee === null) {
+            throw new Error("Attendee not found")
+        }
+
+        return reply.send( { attendee })
+    })
+}
