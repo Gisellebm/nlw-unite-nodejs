38d272b08de6a79894c32ffe8727d36606db1abd
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-04-13 23:06:20 -0300

    Pequenas alterações - final aula 3

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,51 @@
 import { prisma } from '../src/lib/prisma'
+import { faker } from '@faker-js/faker'
+import { Prisma } from '@prisma/client'
+import dayjs from 'dayjs'
 
 async function seed() {
-    await prisma.event.create({
-        data: {
-            id: 'dc86c4e2-0279-4498-b265-4de63e6fcfe6',
-            title: 'Example Event',
-            slug: 'example-event',
-            details: 'This is an example event',
-            maximumAttendees: 120,
+  const eventId = '9e9bd979-9d10-4915-b339-3786b1634f33'
+
+  await prisma.event.deleteMany()
+
+  await prisma.event.create({
+    data: {
+      id: eventId,
+      title: 'Unite Summit',
+      slug: 'unite-summit',
+      details: 'Um evento p/ devs apaixonados(as) por código!',
+      maximumAttendees: 120,
+    }
+  })
+
+  const attendeesToInsert: Prisma.AttendeeUncheckedCreateInput[] = []
+
+  for (let i = 0; i <= 120; i++) {
+    attendeesToInsert.push({
+      id: 10000 + i,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      eventId,
+      createdAt: faker.date.recent({ days: 30, refDate: dayjs().subtract(8, "days").toDate() }),
+      checkIn: faker.helpers.arrayElement<Prisma.CheckInUncheckedCreateNestedOneWithoutAttendeeInput | undefined>([
+        undefined,
+        {
+          create: {
+            createdAt: faker.date.recent({ days: 7 }),
+          }
         }
+      ])
     })
+  }
+
+  await Promise.all(attendeesToInsert.map(data => {
+    return prisma.attendee.create({
+      data,
+    })
+  }))
 }
 
 seed().then(() => {
-    console.log('Seeded database');
-    prisma.$disconnect();
+  console.log('Database seeded!')
+  prisma.$disconnect()
 })
--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -1,66 +1,63 @@
-// serve para retornar o cracha de um participante dentro de um evento
-
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
-import { request } from "http";
 import { BadRequest } from "./_errors/bad-request";
 
 export async function getAttendeeBadge(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
-    .get("/attendees/:attendeeId/badge", {
-        schema: { 
-            summary: "Get attendee badge",
-            tags: ["attendees"],
-            params: z.object({
-                attendeeId: z.coerce.number().int(),
-            }),
-            response: {
-                200: z.object({
-                    badge: z.object({
-                        name: z.string(),
-                        email: z.string().email(),
-                        eventTitle: z.string(),
-                        checkInURL: z.string().url(),
-                    })
-                })
-            },
-        }
+    .get('/attendees/:attendeeId/badge', {
+      schema: {
+        summary: 'Get an attendee badge',
+        tags: ['attendees'],
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            })
+          })
+        },
+      }
     }, async (request, reply) => {
-        const { attendeeId } = request.params
+      const { attendeeId } = request.params
 
-        const attendee = await prisma.attendee.findUnique({
+      const attendee = await prisma.attendee.findUnique({
+        select: {
+          name: true,
+          email: true,
+          event: {
             select: {
-                name: true,
-                email: true,
-                event: {
-                    select: {
-                        title: true,
-                    }
-                }
+              title: true,
             },
-            where: {
-                id: attendeeId
-            }
-        })
-
-        if (attendee === null) {
-            throw new BadRequest("Attendee not found")
+          },
+        },
+        where: {
+          id: attendeeId,
         }
+      })
 
-        const baseURL = `${request.protocol}://${request.hostname}`
+      if (attendee === null) {
+        throw new BadRequest('Attendee not found.')
+      }
 
-        const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+      const baseURL = `${request.protocol}://${request.hostname}`
 
-        return reply.send( {
-            badge: {
-                name: attendee.name,
-                email: attendee.email,
-                eventTitle: attendee.event.title,
-                checkInURL: checkInURL.toString(),
-            }
-         })
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        }
+      })
     })
 }
